165913dabade04ea84f64a207d9fad1b7c67f145
JBYoshi <jbyoshi@users.noreply.github.com>
2018-07-03 14:35:02 +0000

    Add bluetooth-led.py.

---
+++ bluetooth-led.py
@@ -0,0 +1,61 @@
+# The communication format looks like this:
+# '0' - light is off
+# '1' - light is on
+# Send '0' to turn the light off, '1' to turn it on
+# The RPi sends '0' when the light is off, '1' when the light is on
+
+from bluetooth import *
+import RPi.GPIO as GPIO
+
+server_sock = BluetoothSocket(RFCOMM)
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+uuid = "00001101-0000-1000-8000-00805f9b7541"
+
+advertise_service(server_sock, 'JR',
+                  service_id = uuid,
+                  service_classes = [uuid, SERIAL_PORT_CLASS],
+                  profiles = [SERIAL_PORT_PROFILE],
+                 )
+
+on = False
+gpioSlot = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(false)
+GPIO.setup(gpioSlot, GPIO.OUT)
+
+while True:
+    print 'Waiting for connection on RFCOMM'
+    client_sock, client_info = server_sock.accept()
+    print 'Accepted connection from: ', client_info
+    if on:
+        client_sock.send('1')
+    else:
+        client_sock.send('0')
+
+    try:
+        data = client_sock.recv(1)
+        if len(data) == 0: break
+        print 'received [%s]' % data
+        if data == '1':
+            on = True
+            GPIO.output(gpioSlot, GPIO.HIGH)
+        else:
+            data = '0'
+            on = False
+            GPIO.output(gpioSlot, GPIO.LOW)
+        client_sock.send(data)
+        print 'sending [%s]' % data
+
+    except IOError:
+        pass
+
+    except KeyboardInterrupt:
+        print 'disconnected'
+        client_sock.close()
+        server_sock.close()
+        GPIO.output(gpioSlot, GPIO.LOW)
+        print 'all done!'
+        break
+    
